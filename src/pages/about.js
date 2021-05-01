6914b58b1d2fa0778dc87be65a41c3139069155a
HomayounFakhar <homayounfakhar@yahoo.com>
2021-05-01 17:40:10 -0400

    Add About page

---
+++ src/pages/about.js
@@ -0,0 +1,28 @@
+import * as React from "react"
+import { graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const AboutPage = ({ data, location }) => {
+  const siteTitle = data.site.siteMetadata.title
+
+  return (
+    <Layout location={location} title={siteTitle}>
+      <Seo title="About" />
+      <h1>About About About memememe</h1>
+    </Layout>
+  )
+}
+
+export default AboutPage
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
