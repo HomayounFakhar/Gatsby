907084ad04e8a15a821b32f7090ff5232ba1f056
HomayounFakhar <homayounfakhar@yahoo.com>
2021-05-02 14:28:12 -0400

    Change Ordering to ASC

--- src/pages/index.js
+++ src/pages/index.js
@@ -108,7 +108,7 @@ export const pageQuery = graphql`
         title
       }   
     }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: ASC }) {
       nodes {
         excerpt
         fields {
